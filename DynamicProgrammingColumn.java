aeb7e812ed49d1f95990ccf284079016a655df23
Haleema Rustha B A <25cs074@rmd.ac.in>
2026-06-06 13:18:16 +0530

    Add files via upload

---
+++ DynamicProgrammingColumn.java
@@ -0,0 +1,36 @@
+import java.util.*;
+public class DynamicProgrammingColumn {
+    static int maxSum = Integer.MIN_VALUE;
+    static void solve(int[][] a, int col, boolean[] used, int sum) {
+        int n = a.length;
+        if (col == n) {
+            maxSum = Math.max(maxSum, sum);
+            return;
+        }
+        for (int row = 0; row < n; row++) {
+            if (!used[row]) {
+                used[row] = true;
+
+                solve(a, col + 1, used, sum + a[row][col]);
+
+                used[row] = false; // backtrack
+            }
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[][] a = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                a[i][j] = sc.nextInt();
+            }
+        }
+        boolean[] used = new boolean[n];
+        solve(a, 0, used, 0);
+        System.out.println(maxSum);
+    }
+}
---
+++ maximumValuesInAMatrix.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class MaximumValuesInAMatrix {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[][] a = new int[n][n];
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                a[i][j] = sc.nextInt();
+            }
+        }
+        int max = a[0][0];
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                max = Math.max(max, a[i][j]);
+            }
+        }
+        System.out.println(max);
+    }
+}
